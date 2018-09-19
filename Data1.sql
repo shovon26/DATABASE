c713ab4d4f2cc2cbe6729963fb1da932576d609f
shovon26 <36441192+shovon26@users.noreply.github.com>
2018-09-19 16:01:57 +0600

    Create Data1.sql

---
+++ Data1.sql
@@ -0,0 +1,29 @@
+insert into movies values(1,'Rear Window',1954);
+insert into movies values(NULL,'To Catch A Thief',1955);
+insert into movies values(NULL,'The Maltese Falcon',1941);
+insert into movies values(NULL,'The Birds',1963);
+insert into movies values(NULL,'North By North West',1959);
+insert into movies values(NULL,'Casablanka',1942);
+insert into movies values(NULL,'Anatomy of A Murder',1959);
+
+
+insert into persons values(1,'Alfred Hitchcock','M','1899-08-13');
+insert into persons values(NULL,'Cary Grant','M','1904-01-18');
+insert into persons values(NULL,'Grace Kelly','F','1929-11-12');
+insert into persons values(NULL,'Humphery Bogart','M','1899-12-25');
+insert into persons values(NULL,'Sydney Greenstreet','M','1879-12-27');
+insert into persons values(NULL,'James stewart','M','1908-05-20');
+
+
+insert into roles values(1,1,'D');
+insert into roles values(1,3,'A');
+insert into roles values(1,6,'A');
+insert into roles values(2,1,'D');
+insert into roles values(2,2,'A');
+insert into roles values(2,3,'A');
+insert into roles values(3,4,'A');
+insert into roles values(3,5,'A');
+insert into roles values(4,1,'D');
+insert into roles values(5,1,'D');
+insert into roles values(5,2,'A');
+insert into roles values(6,4,'A');
